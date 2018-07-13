d04a09862e5cc5e38a666e84f107fa4e750c70df
kat2018 <kat.vge@gmail.com>
2018-07-13 11:04:37 -0400

    seeded data and created db

---
+++ db/migrate/20180713145443_create_songs.rb
@@ -0,0 +1,12 @@
+class CreateSongs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :preview_url
+      t.string :album
+      t.references :artist, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
